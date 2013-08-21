0fe7d62204ddd9718e0ef5ceaa0f5d38264c4fc7
aboynejames <james@aboynejames.co.uk>
2013-08-21 11:58:34 +0100

    documenation via yuidoc

---
+++ out/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "MasterWatch",
+        "PerSwimmer",
+        "SwimtimeController",
+        "llLogic",
+        "makeProgramme",
+        "pouchdbSettings",
+        "recordQS",
+        "ttHTML"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
